9aec10d243e3019c9af6c134a72b3dfc27692980
Time <98479146+TimeOfMaster@users.noreply.github.com>
2023-12-18 02:04:31 +0100

    added Day-03

---
+++ 2023/Day-03/01.py
@@ -0,0 +1,47 @@
+import os
+from collections import defaultdict
+
+input_path = os.path.dirname(os.path.abspath(__file__)) + r"\\input.txt"
+
+grid = []
+
+with open(input_path, 'r') as f:
+    for line in f.readlines():
+        grid.append(line.strip())
+
+n = len(grid)
+m = len(grid[0])
+
+def get_numbers(i, j1, j2):
+    number_set = set()
+    for j in range(j1, j2):
+        for di in [-1, 0, 1]:
+            for dj in [-1, 0, 1]:
+                if (di, dj) == (0, 0):
+                    continue
+                nbri, nbrj = i + di, j + dj
+                if 0 <= nbri < n and 0 <= nbrj < m:
+                    number_set.add((nbri, nbrj))
+    return number_set
+
+def is_symbol(char):
+    return (not char.isdigit()) and char != '.'
+
+total = 0
+table = defaultdict(list)
+for i in range(n):
+    j = 0
+    while j < m:
+        if not grid[i][j].isdigit():
+            j += 1
+            continue
+        j2 = j + 1
+        while j2 < m and grid[i][j2].isdigit():
+            j2 += 1
+        if any(is_symbol(grid[nbri][nbrj]) for nbri, nbrj in get_numbers(i, j, j2)):
+            num = int(grid[i][j:j2])
+            total += num
+            
+        j = j2
+
+print(total)
