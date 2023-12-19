6981ccf187fbf8801c75cf7b4f1c91d6009c0f39
Time <98479146+TimeOfMaster@users.noreply.github.com>
2023-12-20 00:17:38 +0100

    added Day-16

---
+++ 2023/Day-16/01.py
@@ -0,0 +1,56 @@
+import os
+from dataclasses import dataclass
+
+input_path = os.path.dirname(os.path.abspath(__file__)) + r"\\input.txt"
+
+grid = []
+with open(input_path, 'r') as f:
+    for line in f.readlines():
+        grid.append(line.strip())
+        
+n, m = len(grid), len(grid[0])
+
+@dataclass(frozen=True)
+class Pos:
+    row: int
+    col: int
+    def __add__(self, other):
+        return Pos(row=self.row + other.row, col=self.col + other.col)
+
+def get_num_energised(pos, direction):
+    beams = [(pos, direction)]
+    energised = set()
+    already_done = set()
+    while beams:
+        pos, direction = beams.pop()
+        already_done.add((pos, direction))
+        next_pos = pos + direction
+        if not (0 <= next_pos.row < n and 0 <= next_pos.col < m):
+            continue
+        energised.add(next_pos)
+        char = grid[next_pos.row][next_pos.col]
+        next_directions = []
+        if char == '.':
+            next_directions.append(direction)
+        elif char == '/':
+            next_directions.append({Pos(0, 1): Pos(-1, 0), Pos(1, 0): Pos(0, -1), Pos(-1, 0): Pos(0, 1), Pos(0, -1): Pos(1, 0)}[direction])
+        elif char == '\\':
+            next_directions.append({Pos(0, 1): Pos(1, 0), Pos(1, 0): Pos(0, 1), Pos(-1, 0): Pos(0, -1), Pos(0, -1): Pos(-1, 0)}[direction])
+        elif char == '-':
+            if direction.row == 0:
+                next_directions.append(direction)
+            else:
+                next_directions.append(Pos(0, -1))
+                next_directions.append(Pos(0, 1))
+        elif char == '|':
+            if direction.col == 0:
+                next_directions.append(direction)
+            else:
+                next_directions.append(Pos(1, 0))
+                next_directions.append(Pos(-1, 0))
+        for next_dir in next_directions:
+            if (next_pos, next_dir) not in already_done:
+                beams.append((next_pos, next_dir))
+    return len(energised)
+
+print(get_num_energised(Pos(0, -1), Pos(0, 1)))
