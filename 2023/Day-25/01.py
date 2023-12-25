f2f4c3d1c1be3c46af7bf1f68644e9d7f03d78b9
Time <98479146+TimeOfMaster@users.noreply.github.com>
2023-12-25 17:48:54 +0100

    🧑‍💻 make code more readable

--- 2023/Day-25/01.py
+++ 2023/Day-25/01.py
@@ -11,15 +11,15 @@ finally:
 
 input_path = os.path.dirname(os.path.abspath(__file__)) + r"\\input.txt"
 
-g = nx.Graph()
+graph = nx.Graph()
 
 for line in open(input_path, 'r'):
     left, right = line.split(":")
     for node in right.strip().split():
-        g.add_edge(left, node)
-        g.add_edge(node, left)
+        graph.add_edge(left, node)
+        graph.add_edge(node, left)
 
-g.remove_edges_from(nx.minimum_edge_cut(g))
-a, b = nx.connected_components(g)
+graph.remove_edges_from(nx.minimum_edge_cut(graph))
+a, b = nx.connected_components(graph)
 
 print(len(a) * len(b))
